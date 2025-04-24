f15b0e7c7a71c31bb3286b3d49c92babb4f0cb9e
raycc51 <4q26dd@gmail.com>
2025-04-24 09:13:19 +0900

    version, license

--- bw.sh
+++ bw.sh
@@ -40,7 +40,7 @@ CUSTOM_HTML_HEAD=""
 
 # script info
 _SCRIPT_NAME='BashWrite'
-_SCRIPT_VERSION='1.1.4'
+_SCRIPT_VERSION='1.1.5'
 _SCRIPT_FILE_NAME='bw.sh'
 _SCRIPT_SITE='https://github.com/raycc51/bashwrite'
 
