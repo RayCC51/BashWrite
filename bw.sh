2200df5f7ad0b391fc21867fd61b5ddf4e5fc97c
raycc51 <4q26dd@gmail.com>
2025-04-27 10:04:22 +0900

    feat: add giscus comments

--- bw.sh
+++ bw.sh
@@ -16,7 +16,7 @@ LANG="en"
 
 ### How many recent posts should be shown on the homepage.
 ### Set it to 0 to hide recent posts.
-RECENT_POSTS_COUNT=5
+RECENT_POSTS_COUNT=10
 
 ### Write your profile in Markdown syntax.
 ### This paragraph will be included in the homepage of your blog.
@@ -56,7 +56,9 @@ CUSTOM_HTML_HEAD="
 # </body>
 ### only in the posts.
 ### You can add comments, banner, footer, or js.
-CUSTOM_HTML_ARTICLE_FOOTER=""
+CUSTOM_HTML_ARTICLE_FOOTER="
+<script src=\"https://giscus.app/client.js\" data-repo=\"RayCC51/BashWrite\" data-repo-id=\"R_kgDOOQp0-w\" data-category=\"Announcements\" data-category-id=\"DIC_kwDOOQp0-84CpMGk\" data-mapping=\"pathname\" data-strict=\"0\" data-reactions-enabled=\"1\" data-emit-metadata=\"0\" data-input-position=\"top\" data-theme=\"light\" data-lang=\"en\" data-loading=\"lazy\" crossorigin=\"anonymous\" async> </script>
+"
 
 
 #
