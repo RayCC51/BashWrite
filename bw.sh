43ab8fe5a3e3aee2f3f957496130197b828b29c6
raycc51 <4q26dd@gmail.com>
2025-04-27 09:57:00 +0900

    chore, add contributing in readme, fix favicon link

--- bw.sh
+++ bw.sh
@@ -37,10 +37,10 @@ You can find the source of this blog in the [Github](https://github.com/RayCC51/
 ### in every html files.
 ### You can add your css, js, or favicon. 
 CUSTOM_HTML_HEAD="
-<link rel=\"apple-touch-icon\" sizes=\"180x180\" href=\"/assets/favicon_io/apple-touch-icon.png\">
-<link rel=\"icon\" type=\"image/png\" sizes=\"32x32\" href=\"/assets/favicon_io/favicon-32x32.png\">
-<link rel=\"icon\" type=\"image/png\" sizes=\"16x16\" href=\"/assets/favicon_io/favicon-16x16.png\">
-<link rel=\"manifest\" href=\"/assets/favicon_io/site.webmanifest\">
+<link rel=\"apple-touch-icon\" sizes=\"180x180\" href=\"$BASE_URL/assets/favicon_io/apple-touch-icon.png\">
+<link rel=\"icon\" type=\"image/png\" sizes=\"32x32\" href=\"$BASE_URL/assets/favicon_io/favicon-32x32.png\">
+<link rel=\"icon\" type=\"image/png\" sizes=\"16x16\" href=\"$BASE_URL/assets/favicon_io/favicon-16x16.png\">
+<link rel=\"manifest\" href=\"$BASE_URL/assets/favicon_io/site.webmanifest\">
 "
 
 ### Write your own HTML code.
